54ab9f68e5f68dc9a4ea2eea1c219edde7c29699
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-09-02 17:01:33 +0100

    find() JS

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(win) {
+  let res = win.find(function(object) {return object.result === "W"})
+  if (res) {
+    return res.year
+  } else {
+    return undefined
+  }
+}
