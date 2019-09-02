f8773e6db953e339bf358edcdfd308006636e52f
eliothowes1090 <ejhowes@gmail.com>
2019-09-02 20:54:54 +0100

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = array => {
+  const recordWin = array.find(record => record['result'] === 'W')
+  if (recordWin) {
+    return recordWin.year
+  }
+}
