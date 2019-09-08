a347a6fae73f925829695c95e6bf546f6d4ccee9
Harry Chopra <harrychopra@gmail.com>
2019-09-08 14:39:02 +0100

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-const testVar = {}
+// const testVar = {};
 
-function testFunc() {
-  return "hi"
-}
+// function testFunc() {
+// 	return 'hi';
+// }
+
+// [1, 2, 3, 4, 5].find((e) => {
+// 	return e === 5;
+// });
+// //5
+
+// let roommates = ['jess', 'winston', 'winston', 'nick'];
+// roommates.find((name) => name === 'winston');
+// roommates.filter((name) => {
+// 	return name === 'winston';
+// });
+
+const record = [
+	{year: '2018', result: 'L'},
+	{year: '2017', result: 'W'},
+	{year: '2016', result: 'N/A'}
+	//...
+];
+
+const superbowlWin = (arr) => {
+	const win = arr.find((o) => o.result === 'W');
+	if (win) return win.year;
+};
--- test/helpers.js
+++ test/helpers.js
@@ -1,20 +1,25 @@
-const chai = require('chai')
-global.expect = chai.expect
-const fs = require('file-system')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
+const chai = require('chai');
+global.expect = chai.expect;
+const fs = require('file-system');
+const jsdom = require('mocha-jsdom');
+const path = require('path');
 const babel = require('babel-core');
 
-const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
-
-const babelResult = babel.transformFileSync(
-  path.resolve(__dirname, '..', 'index.js'), {
-    presets: ['env']
-  }
+const html = fs.readFileSync(
+	path.resolve(__dirname, '..', 'index.html'),
+	'utf-8'
 );
 
-const src = babelResult.code
+const babelResult = babel.transformFileSync(
+	path.resolve(__dirname, '..', 'index.js'),
+	{
+		presets: ['env']
+	}
+);
+
+const src = babelResult.code;
 
 jsdom({
-  html, src
+	html,
+	src
 });
